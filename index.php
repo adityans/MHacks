06cce4e0d83228c0ee1c0b36d59e078e939878c1
System Administrator <root@67-194-67-148.wireless.umnet.umich.edu>
2013-09-21 03:10:17 -0400

    update

--- index.php
+++ index.php
@@ -3,4 +3,10 @@
 
 echo "<h1>Fitness Alert</h3><br />";
 
+echo "<a href='https://runkeeper.com/apps/authorize?client_id=aacc1328145f4cccb62ec484e21c4704&redirect_uri=http%3A%2F%2Flocalhost%2FMHacks%2Findex.php&response_type=code&state='><img src='http://static1.runkeeper.com/images/assets/connect-blue-white-200x38.png'/></a>";
+
+
+curl_setopt($ch, CURLOPT_POST, false);
+
+
 ?>

6a2582ebd3820ca05991a00117db29ab8eb02857
System Administrator <root@67-194-67-148.wireless.umnet.umich.edu>
2013-09-21 14:22:27 -0400

    alksd

--- checkin.php
+++ checkin.php
@@ -64,7 +64,33 @@ if ($count !=$weekgoal)
  	$response = $facebook->api('me/feed', 'POST', $post);
  	print_r($response);
 
+	// Include the Twilio PHP library
+	require 'Services/Twilio.php';
 
+	// Twilio REST API version
+	$version = "2010-04-01";
+
+	// Set our Account SID and AuthToken
+	$sid = 'ACf3dbda4ec2c32335fcf5d71f821abfeb';
+	$token = '257bb8ae333156a4fc2c2702a5737c33';
+	
+	// A phone number you have previously validated with Twilio
+	$phonenumber = '9785183274';
+	
+	// Instantiate a new Twilio Rest Client
+	$client = new Services_Twilio($sid, $token, $version);
+
+	try {
+		// Initiate a new outbound call
+		$call = $client->account->calls->create(
+			$phonenumber, // The number of the phone initiating the call
+			'2484100614', // The number of the phone receiving call
+			'https://5563754a.ngrok.com/MHacks/playmp3.php' // The URL Twilio will request when the call is answered
+		);
+		echo 'Started call: ' . $call->sid;
+	} catch (Exception $e) {
+		echo 'Error: ' . $e->getMessage();
+	}
 
 
 }
